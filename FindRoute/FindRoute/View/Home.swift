2966b45d670947604f79bd3fab33da7b20da6afd
Anton Biryukov <109631375+vokuyriBnotnA@users.noreply.github.com>
2022-08-29 16:51:56 +0300

    Add files via upload

---
+++ FindRoute/FindRoute/View/Home.swift
@@ -0,0 +1,127 @@
+//
+//  Home.swift
+//  FindRoute
+//
+//  Created by Artem Serebriakov on 24.08.2022.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Home: View {
+    
+    @StateObject var mapData = MapViewModel()
+    @State var locationManager = CLLocationManager()
+    
+    var body: some View {
+        ZStack {
+            MapView()
+                .environmentObject(mapData)
+                .ignoresSafeArea(.all, edges: .all)
+            VStack {
+                
+                VStack(spacing: 0) {
+                    HStack {
+                        
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        
+                        TextField("Search", text: $mapData.searchTxt)
+                            .colorScheme(.light)
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal)
+                    .background(Color.white)
+                    
+                    if !mapData.places.isEmpty && mapData.searchTxt != "" {
+                        
+                        ScrollView {
+                            
+                            VStack(spacing: 15) {
+                                ForEach(mapData.places) { place in
+                                    Text(place.place.name ?? "" )
+                                        .foregroundColor(.black)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .padding(.leading)
+                                        .onTapGesture {
+                                            mapData.selectPlace(place: place)
+                                        }
+                                    
+                                    Divider()
+                                }
+                            }
+                            .padding(.top)
+                        }
+                        .background(Color.white)
+                    }
+                }
+                .padding()
+                
+                Spacer()
+                
+                VStack {
+                    
+                    Button(action: mapData.focusLocation, label: {
+                        
+                        Image(systemName: "location.fill")
+                            .font(.title2)
+                            .padding(10)
+                            .background(Color.primary)
+                            .clipShape(Circle())
+                    })
+                    
+                    Button(action: mapData.updateMapType, label: {
+                        
+                        Image(systemName: mapData.mapType == .standard ? "network" : "map")
+                            .font(.title2)
+                            .padding(10)
+                            .background(Color.primary)
+                            .clipShape(Circle())
+                    })
+                    
+                    Button(action: mapData.updateTransportType, label: {
+                        
+                        Image(systemName: mapData.mapTransportType == .automobile ? "car.circle" : "figure.walk.circle")
+                            .font(.title2)
+                            .padding(10)
+                            .background(Color.primary)
+                            .clipShape(Circle())
+                    })
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding()
+            }
+        }
+        .onAppear(perform: {
+            locationManager.delegate = mapData
+            locationManager.requestWhenInUseAuthorization()
+        })
+        .alert(isPresented: $mapData.permissionDenied, content: {
+            
+            Alert(title: Text("Premission Denied"), message: Text("Please enable permission in app settings"), dismissButton: .default(Text("Go to Settings"), action: {
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            }))
+        })
+        .onChange(of: mapData.searchTxt, perform: { value in
+            let delay = 0.3
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                
+                if value == mapData.searchTxt {
+                    self.mapData.searchQuery()
+                }
+            }
+        })
+        .alert(isPresented: $mapData.cannotRoute, content: {
+            
+            Alert(title: Text("Directions Not Available"), message: Text("Directions are not available between these location"), dismissButton: .default(Text("ok")))
+        })
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
---
+++ FindRoute/FindRoute/ViewModel/MapViewModel.swift
@@ -0,0 +1,138 @@
+//
+//  MapViewModel.swift
+//  FindRoute
+//
+//  Created by Artem Serebriakov on 24.08.2022.
+//
+
+import SwiftUI
+import MapKit
+import CoreLocation
+
+class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var mapView = MKMapView()
+    @Published var region: MKCoordinateRegion!
+    @Published var permissionDenied = false
+    @Published var cannotRoute = false
+    @Published var mapType: MKMapType = .standard
+    @Published var mapTransportType: MKDirectionsTransportType = .automobile
+    @Published var searchTxt = ""
+    @Published var places: [Place] = []
+    
+    func updateTransportType() {
+        if mapTransportType == .automobile {
+            mapTransportType = .walking
+        } else {
+            mapTransportType = .automobile
+        }
+    }
+    
+    func updateMapType() {
+        if mapType == .standard {
+            mapType = .hybrid
+            mapView.mapType = mapType
+        } else {
+            mapType = .standard
+            mapView.mapType = mapType
+        }
+    }
+    
+    func focusLocation() {
+        guard let _ = region else { return }
+        
+        mapView.setRegion(region, animated: true)
+        mapView.setVisibleMapRect(mapView.visibleMapRect, animated: true)
+    }
+    
+    func searchQuery() {
+        places.removeAll()
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchTxt
+        
+        MKLocalSearch(request: request).start { (response, _) in
+            
+            guard let result = response else { return }
+            
+            self.places = result.mapItems.compactMap({ (item) -> Place? in
+                return Place(place: item.placemark)
+            })
+        }
+    }
+
+    func selectPlace(place: Place) {
+        self.cannotRoute = false
+        searchTxt = ""
+        
+        guard let coordinate = place.place.location?.coordinate else { return }
+        
+        let pointAnnotstion = MKPointAnnotation()
+        pointAnnotstion.coordinate = coordinate
+        pointAnnotstion.title = place.place.name ?? "No name"
+        
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(pointAnnotstion)
+        
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        
+        let p1 = MKPlacemark(coordinate: coordinate)
+        let p2 = MKPlacemark(coordinate: region.center)
+
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: p1)
+        request.destination = MKMapItem(placemark: p2)
+        request.transportType = mapTransportType
+
+        let directions = MKDirections(request: request)
+        directions.calculate { response, error in
+            guard let route = response?.routes.first else { self.cannotRoute = true; return }
+//            self.mapView.addAnnotations([p1,p2])
+            self.mapView.addOverlay(route.polyline)
+            self.mapView.setVisibleMapRect(
+                route.polyline.boundingMapRect,
+                edgePadding: UIEdgeInsets(top: 100, left: 100, bottom: 100, right: 100),
+                animated: true
+            )
+        }
+        
+        
+        for overlay in mapView.overlays {
+
+             mapView.removeOverlay(overlay)
+         }
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+        mapView.setVisibleMapRect(mapView.visibleMapRect, animated: true)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .denied:
+            //alert
+            permissionDenied.toggle()
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .authorizedWhenInUse:
+            manager.requestLocation()
+        default:
+            ()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let location = locations.last else { return }
+        
+        self.region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        
+        self.mapView.setRegion(self.region, animated: true)
+        
+        self.mapView.setVisibleMapRect(self.mapView.visibleMapRect, animated: true)
+    }
+}
+
